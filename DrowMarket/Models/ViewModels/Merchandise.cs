f78cf2ace744822fb011a26bda01478cef1d01a6
BramPWijshake <BrampWijshake@gmail.com>
2020-05-08 09:00:39 +0200

    References en Merchandise toegevoegd

---
+++ DrowMarket/Models/ViewModels/Merchandise.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Merchandise
+    {
+        string Name { get; set; }
+        double Value { get; set; }
+        string Picture { get; set; }
+        int InStock { get; set; }
+    }
+}
